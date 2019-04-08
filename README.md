d312edfabbbc7d5dc6a714e9cbc8f45c0a1d112e
Brian Smith <bsmith@Brians-MBP.fios-router.home>
2019-04-07 22:40:54 -0400

    added ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# bamazon
-Storefront CLI App
+# bamazon - A Pro Video and Audio Equipment Store
+
+## Video Example
+https://drive.google.com/file/d/1cBRd5JFAdYIX6aYTqKh7Lv0n63AB6Mz_/view
+
+## Description
+This command line tool uses a MySQL Database to track inventory of items available for sale.  The user will type the Product ID and the Quantity to purchase an item.  Once the purchase is complete, the inventory will be updated based on the purchase order quantity.  
+
+## Commands
+Initialize application to see the current list of inventory with the command line below, while in application directory:
+* node bamazon_Customer
+
+## Prompts
+The tool will first show the user all of the available products for sale in the store including ID and Price.  The user will then be prompted to select
+* `Item ID`
+* `Quantity`
+
+## Purchase Confirmation
+Once Product ID and quantity have been selected & the item is in stock bamazon will provide the following information:
+
+1. Confirm order has been submitted
+2. Show order total based on product ID and selected quantity
+3. Show how many of the Item remain in Stock
+
+Once completed the user will be prompted to select another product ID to purchase
+
+## Inventory
+If there is not enough stock to complete the order bamazon will provide the following information:
+
+1. Alert that there is not enough stock to complete the order
+2. Prompt to select another product ID to purchase
+
+## Technologies Incorporated
+* MySQL
+* Sequel Pro
+* MAMP
+* node.js
+  * Inquirer
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,239 +1,123 @@
+//package requirements
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 8889,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "root",
-  database: "bamazon_DB"
-});
+    //database connection info
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "bamazon_DB"
+    });
 
 //inital database connection and display inventory
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected to database\n");
-  showItems();
-});
-
-function runSearch() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "Find songs by artist",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song",
-        "exit"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
-        break;
-
-      case "Find all artists who appear more than once":
-        multiSearch();
-        break;
-
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
-
-      case "Search for a specific song":
-        songSearch();
-        break;
-
-      case "exit":
-        connection.end();
-        break;
-      }
+        connection.connect(function(err) {
+            if (err) throw err;
+                console.log("connected to database\n");
+        showItems();
     });
-}
 
+//buy item function
 function buyItem() {
-  inquirer
-    .prompt([{
-      name: "id",
-      type: "input",
-      message: "Enter the product ID you wish to buy",
-    //   validate: function(value) {
-    //     if (isNaN(value) === false) {
-    //       return true;
-    //     }
-    //     return false;
-    //   }
-    },
-    {
-        name: "quantity",
-        type: "input",
-        message: "How many you need?"
-      }])
+            inquirer.prompt([{
+            name: "id",
+            type: "input",
+            message: "Enter the product ID you wish to buy",
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many?"
       //END array of questions
+        }])
 
-    .then(function(answer) {
-    //   var query = 'SELECT COUNT(*) FROM top5000 WHERE ?';
-    //   connection.query(query, { artist: answer.artist }, function(err, res) {
-        // for (var i = 0; i < res.length; i++) {
-        //   console.log("Position: " + res[i].position);
-        //   console.log("Song: " + res[i].song)
-        //   console.log("Year: " + res[i].year);
-        // }
-        console.log("checking answer object");
-        console.log(answer);
+    //Callback function query DB
+         .then(function(answer) {
 
-        var query = 'SELECT * FROM bamazon_DB.products WHERE ?';
+        // console.log("checking answer object");
+        // console.log(answer);
+            var query = 'SELECT * FROM bamazon_DB.products WHERE ?';
 
-        connection.query(query, { id: answer.id }, function(error, response){
+            connection.query(query, { id: answer.id }, function(error, response){
 
-       
-            console.log(response);
+       //test database query response - array of objects
+        // console.log(response);
+
             console.log("item qty is " + response[0].quantity);
+
+            //check item stock against database
             if (answer.quantity <= response[0].quantity){
 
                 console.log("We have enough in stock");
                 //fulfill order here
                 
                 var remainingQty = response[0].quantity - answer.quantity;
-                console.log("remaining Qty for " + response[0].product_name + " is " + remainingQty);
+                var orderTotal = answer.quantity * response[0].price;
 
-                // connection.query(
-                //     '"UPDATE bamazon_DB.products SET quantity =" + remainingQty + " WHERE id=" + answer.id';
-                //     {
-                //       item_name: answer.item,
-                //       category: answer.category,
-                //       starting_bid: answer.startingBid || 0,
-                //       highest_bid: answer.startingBid || 0
-                //     },
+                console.log("Remaining stock for " + response[0].product_name + " is " + remainingQty);
+                console.log("You order total is $" + orderTotal);
 
-                // connection.query(query, [{ id: answer.id }, { answer. }], function(error, response){
-                    // "UPDATE bamazon_DB.products WHERE ? SET ?",
-                    // {
-                    //   quantity: response[0].quantity-answer.quantity
-                    // //   category: answer.category,
-                    // //   starting_bid: answer.startingBid || 0,
-                    // //   highest_bid: answer.startingBid || 0
-                    // },
-                
+                connection.query(
+                    "UPDATE bamazon_DB.products SET ? WHERE ?",
+                    [
+                      {
+                        quantity: remainingQty
+                      },
+                      {
+                        id: answer.id
+                      }
+                    ],
 
-            }
-            else{
-                console.log("out of stock");
-            }
+                    function(error) {
+
+                        if (error) throw err;
+                            console.log("Thanks for your order!");
+                            //END Program
+                            console.log("\n");
+                            buyItem();
+                            }
+                )} //END if statement stock check
+                        else{
+                            console.log("Selected Item Quantity is Out of Stock");
+                            //Return to item list
+                            console.log("\n");
+                            buyItem();
+                            }
         
-        })
-        // runSearch();
-    });
-    // });
-}
+            }) //END Query
+        
+        }) //END Callback function then
+ 
+    }; //END buyItem Function
 
-
-//display inventory
+//display inventory function
 function showItems() {
-  var query = 'SELECT * FROM bamazon_DB.products';
-  connection.query(query, function(error, result) {
+    
+        var query = 'SELECT * FROM bamazon_DB.products';
+        
+        connection.query(query, function(error, result) {
 
-    console.log("CURRENT PRODUCT INVENTORY\n");
+                console.log("CURRENT PRODUCT INVENTORY\n");
 
-    for (var i = 0; i < result.length; i++) {
-   
-    console.log("-----------------------------------------"); 
-    console.log(
-        "Product: " + result[i].product_name + "\n" +
-        "Price $: " + result[i].price + "\n" +
-        "ID: " + result[i].id
-      );
-    }
-    //NEXT FUNCTION
-    // runSearch();
-    console.log("calling inquirer function");
-    buyItem();
-  });
+            for (var i = 0; i < result.length; i++) {
+    
+                console.log("-----------------------------------------"); 
+                console.log(
+                    "Product: " + result[i].product_name + "\n" +
+                    "Price $: " + result[i].price + "\n" +
+                    "ID: " + result[i].id
+                );
+            }
 
-};
+        // console.log("calling inquirer function");
+        buyItem();
 
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting year: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending year: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      var query = 'SELECT position, song, artist, year FROM top5000 WHERE year BETWEEN ? AND ?'
-      //' SELECT position, song, artist, year FROM top5000 WHERE year BETWEEN 2009 AND 2011'
-      // [obj, obj]
-      // [2009, 2011]
-      connection.query(query, [answer.start, answer.end], function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
-}
+        }); //END Query
 
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to look for?"
-    })
-    .then(function(answer) {
-      console.log(answer.song);
+    }; //END showItems fn
 
-      var q = connection.query("SELECT * FROM top5000 WHERE ?", { year: answer.song }, function(err, res) {
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
-        console.log(q.sql);
-        runSearch();
-      });
-    });
-}
+
+function mainMenu(){
+
+} 
